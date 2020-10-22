7abdc270c7f2fce8df456bbbcf6075899199271a
Cedric Chin <cedric@elijames.org>
2020-10-22 22:40:50 +0800

    Add ingredients table to webapp.

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -32,13 +32,14 @@ function callPredict(lang) {
       hideInfoBar();
       return
     }
-    //document.getElementById('result').innerHTML=data['hip'];
+    // document.getElementById('result').innerHTML=genIngTable(res);
+    $('#result').append(genIngTable(data['res']));
     document.getElementById('prodpic').src = data['filename'];
     document.getElementById('prodpic').style.display = "inline-block";
     hideInfoBar();
   })
     .catch(error => {
-      document.getElementById('result').innerHTML="";
+      $('#result').empty();
       document.getElementById('prodpic').src = "";
       document.getElementById('prodpic').style.display = "none";
       hideInfoBar();
@@ -46,6 +47,42 @@ function callPredict(lang) {
     });
 }
 
+// GENERATE TABLE
+function genIngTable(res) {
+  if (!res || res.length < 1) {
+    return "";
+  }
+  var table = $("<table></table>").addClass(['collapse', 'ba', 'br2', 'b--black-10', 'pv2', 'ph3', 'bg-white']);
+  var thead = $(`<thead><tr>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Name</th>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Description</th>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Irritancy</th>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Comedogenicity</th>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Rating</th>
+      <th class='pv2 ph3 tl f6 fw6 ttu'>Functions</th>
+    </tr></thead>`);
+      // <th class='pv2 ph3 tl f6 fw6 ttu'>Quick Facts</th>
+  table.append(thead)
+
+  var tbody = $('<tbody></tbody>')
+  var curr, cnode, cth;
+  for (var i=0;i<res.length;i++) {
+    curr = res[i];
+    cnode = $('<tr></tr>').addClass(['striped--light-gray', 'f6']);
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Ingredient_name']));
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Description']));
+    //cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Quick_facts']));
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Irritancy']));
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Comedogenicity']));
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Rating']));
+    cnode.append($('<td></td>').addClass(['pv2', 'ph3']).text(curr['Functions']));
+    tbody.append(cnode);
+  }
+  table.append(tbody);
+  return table;
+}
+
+// INFO BAR CODE - bottom of the screen
 const infobar = document.getElementById('infobar');
 function showInfoBar() {
   infoBarTxtElem.innerHTML = "Uploading image ...";
